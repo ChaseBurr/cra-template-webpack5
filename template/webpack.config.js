fa70ea358291dc16fe2fe245625b4217799b577d
chaseburr <chase@chaseburr.com>
2022-02-08 12:27:32 -0700

    updated webpack config

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,49 +1,62 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-     entry: path.join(__dirname, "src", "index"),
-     output: {
-          path: path.join(__dirname, "dist"),
-          publicPath: "/dist/",
-          filename: "bundle.js",
-          chunkFilename: "[name].js",
-     },
-     module: {
-          rules: [
-               {
-                    test: /.j(s|sx)?$/,
-                    include: [path.resolve(__dirname, "src")],
-                    exclude: [path.resolve(__dirname, "node_modules")],
-                    loader: "babel-loader",
-                    options: {
-                         presets: ["@babel/preset-env"],
-                    },
-               },
-               {
-                    test: /\.(png|jpe?g|svg)$/i,
-                    use: [
-                         {
-                              loader: "file-loader",
-                         },
-                    ],
-               },
-               {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
-               },
-          ],
-     },
-     resolve: {
-          extensions: [".json", ".js", ".jsx"],
-     },
-     devtool: "source-map",
-     plugins: [
-          new HtmlWebpackPlugin({
-               template: path.resolve(__dirname, "public/index.html"),
-               filename: "index.html",
-          }),
-          new Dotenv(),
-     ],
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["autoprefixer", {}]],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg)$/i,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
 };
